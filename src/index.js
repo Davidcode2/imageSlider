bd8e2ebcc674e960521bef1c1a10bb992926f720
Jakob Lingel <jlingel68@gmail.com>
2022-10-02 00:10:56 +0200

    Offset solution, hide or show arrows

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-import './css/style.css'
+import './css/style.css';
+
+class Slider {
+  slider = document.querySelector('.slider');
+
+  currentChild = document.querySelector('.slider > img');
+
+  previousButton = document.querySelector('.previous');
+
+  nextButton = document.querySelector('.next');
+
+  nextImage() {
+    this.nextButton.addEventListener('click', () => {
+      this.currentChild = this.currentChild.nextElementSibling;
+      this.hideOrShowNextPrevButtons();
+      this.currentChild.scrollIntoView();
+    });
+  }
+
+  prevImage() {
+    if (!this.currentChild.previousElementSibling) {
+      this.previousButton.style.visibility = 'hidden';
+    }
+    this.previousButton.addEventListener('click', () => {
+      this.currentChild = this.currentChild.previousElementSibling;
+      this.hideOrShowNextPrevButtons();
+      this.currentChild.scrollIntoView();
+    });
+  }
+
+  hideOrShowNextPrevButtons() {
+    if (!this.currentChild.nextElementSibling) {
+      this.nextButton.style.visibility = 'hidden';
+    } else {
+      this.nextButton.style.visibility = 'visible';
+    }
+    if (!this.currentChild.previousElementSibling) {
+      this.previousButton.style.visibility = 'hidden';
+    } else {
+      this.previousButton.style.visibility = 'visible';
+    }
+  }
+}
+
+const mySlider = new Slider();
+mySlider.nextImage();
+mySlider.prevImage();
